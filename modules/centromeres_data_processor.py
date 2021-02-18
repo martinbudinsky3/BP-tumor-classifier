8c85e76fa07c7f102212eed5e602ebe4d2217dd2
Martin Budinský <martinbudinsky3@github.com>
2021-02-18 18:17:22 +0100

    counting dna index

--- modules/centromeres_data_processor.py
+++ modules/centromeres_data_processor.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def process_centromeres_data(centromeres):
     centromeres = centromeres[centromeres['type'] == 'centromere']
-    centromeres = centromeres.loc[:, 'chrom':'chromEnd']
-    centromeres.rename(columns = { 'chrom': 'Chromosome', 'chromStart': 'Start', 'chromEnd': 'End' }, inplace = True)
+    centromeres = centromeres.loc[:, ['chrom', 'chromStart','chromEnd', 'size']]
+    centromeres.rename(columns = { 'chrom': 'Chromosome', 'chromStart': 'Start', 'chromEnd': 'End', 'size': 'Length' }, inplace = True)
     centromeres.iloc[:, 0] = centromeres.loc[:, 'Chromosome'].apply(lambda x: x.replace('chr', ''))
     centromeres = centromeres.reset_index(drop=True)
     
