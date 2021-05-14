6c085a93f613936205689edb245fb3395d743bfd
Martin Budinský <martinbudinsky3@github.com>
2021-05-15 01:50:31 +0200

    fix in tai

--- hrdtools/tai.py
+++ hrdtools/tai.py
@@ -1,6 +1,7 @@
-from .utils import init_lengths, init_chromosome_names, Mb
+from .utils import init_lengths, init_centromeres, init_chromosome_names, Mb
 
 lengths = init_lengths()
+centromeres = init_centromeres()
 chromosome_names = init_chromosome_names()
 
 def tai(data, TELOMERE_SIZE=2*Mb):
@@ -8,8 +9,13 @@ def tai(data, TELOMERE_SIZE=2*Mb):
     for _chr in chromosome_names:
         chr_data = data.loc[data['Chromosome'] == _chr]
         chr_len = lengths.loc[_chr, 'Length']
-
-        tais_segments = chr_data.loc[(chr_data['Start'] < TELOMERE_SIZE) | (chr_data['End'] > chr_len - TELOMERE_SIZE)]
+        centromere_start = centromeres.loc[centromeres['Chromosome'] == _chr, 'Start'].iloc[0]
+        centromere_end = centromeres.loc[centromeres['Chromosome'] == _chr, 'End'].iloc[0]
+        
+        tais_segments = chr_data.loc[
+            (chr_data['Start'] < TELOMERE_SIZE) & (chr_data['End'] <= centromere_start) | 
+            (chr_data['End'] > chr_len - TELOMERE_SIZE) & (chr_data['Start'] >= centromere_end)
+        ]
         ntai += len(tais_segments.index)
 
     return ntai
